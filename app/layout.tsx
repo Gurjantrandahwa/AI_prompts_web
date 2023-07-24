1798c1ed6e09bd4d1df3a70f7ac61fd6d489d422
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-07-24 21:25:09 +0530

    Next Js AI prompts

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Prompts Web",
+    description: "Discover and share the AI Prompts"
+}
+const Layout = () => {
+    return <div>
+        Layout
+    </div>
+};
+
+export default Layout;
