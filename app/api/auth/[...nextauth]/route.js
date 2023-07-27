7bbeee26b2a02f060f077bf20b9ec17dd183af7b
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-07-27 13:13:29 +0530

    Next Js AI prompts

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,7 +5,7 @@ import User from '../../../../models/user';
 
 
 console.log({
-    contentId: process.env.GOOGLE_ID,
+    clientId: process.env.GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 })
 
@@ -13,42 +13,40 @@ console.log({
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            contentId: process.env.GOOGLE_ID,
+            clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    callbacks: {
+        async session({session}) {
+            // store the user id from MongoDB to session
+            const sessionUser = await User.findOne({email: session.user.email});
+            session.user.id = sessionUser._id.toString();
 
-    async session({session}) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-        session.user.id = sessionUser._id.toString();
-        return session
-    },
+            return session;
+        },
+        async signIn({ profile}) {
+            try {
+                await connectToDB();
+                // check if user already exists
+                const userExists = await User.findOne({email: profile.email});
 
-    async signIn({profile}) {
-        try {
-            await connectToDB();
+                // if not, create a new document and save user in MongoDB
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.image,
+                    });
+                }
 
-            // check if user already exists
-            const userExits = await User.findOne({
-                email: profile.email
-            })
-            //if not create a new user
-            if (!userExits) {
-                await User.create({
-                    email: profile.email,
-                    userName: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
-                })
+                return true
+            } catch (error) {
+                console.log("Error checking if user exists: ", error.message);
+                return false
             }
-            return true;
-
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    },
+        },
+    }
 })
 
-export {handler as GET, handler as POST};
+export {handler as GET, handler as POST}
--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental:{
-        appDir:true,
-        serverComponentsExternalPackages:["mongoose"],
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
     },
-    images:{
-        domains:['1h3.googleusercontent.com'],
+    images: {
+        domains: ['lh3.googleusercontent.com'], // Add the domain for your remote images here
+        loader: 'default', // Or use your preferred image loader
     },
-    webpack(config){
-        config.experiments={
+    webpack(config) {
+        config.experiments = {
             ...config.experiments,
-            topLevelAwait:true,
-        }
-        return config
+            topLevelAwait: true,
+        };
+        return config;
     }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
